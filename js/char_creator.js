b283e39c4e0c297db5c67fb2ae4728fa3cead206
Holly <hbedard@live.ca>
2020-11-29 14:19:04 -0500

    Update html to include fieldsets; More detailed commenting; Reorganized JS file

--- js/char_creator.js
+++ js/char_creator.js
@@ -1,19 +1,24 @@
 /*
 Main Author:  Stephanie Holly Bedard, 200443133
 */
+
+/*******************************************************************************************/
+/*                                     UTILITIES                                           */
+/*******************************************************************************************/
+
 //creates the initial page configurations
 function startup() {
 
     //get values for the Race dropdown
-    var racesArray;
-    fetch('https://www.dnd5eapi.co/api/races')
-        .then(res => res.json())
-        .then(data => racesArray = data.results)
-        .then(() => racesArray.forEach(function(race){createDropdown(race,'race_input')}))
+    var racesArray;  //initialize variable to store race data in
+    fetch('https://www.dnd5eapi.co/api/races')//connect to API to retrieve race data
+        .then(res => res.json())    //convert/parse api data to json
+        .then(data => racesArray = data.results)   //get the race data needed from data array and store in variable
+        .then(() => racesArray.forEach(function(race){createDropdown(race,'race_input')}))  //create a dropdown with each race name from the race array as an option
 
     //get values for the Gender dropdown
-    const gendersArray = [{name:"Female"},{name: "Male"}];
-    gendersArray.forEach(function(gender){createDropdown(gender,'gender_input')})
+    const gendersArray = [{name:"Female"},{name: "Male"}];  //declare array with gender types, with json format
+    gendersArray.forEach(function(gender){createDropdown(gender,'gender_input')})  //create a dropdown with each gender name from the gender array as an option
 
     //get values for the Class dropdown
     var classesArray;
@@ -24,57 +29,120 @@ function startup() {
 
     //get values for Background dropdown
     var backgroundsArray;
-    fetch("../json/backgrounds.json")
-        .then(res => res.json())
-        .then(data => backgroundsArray = data.backgrounds)
-        .then(() => backgroundsArray.forEach(function(background){createDropdown(background,'background_input')}))
+    fetch("../json/backgrounds.json") //retrieve data from json file
+        .then(res => res.json()) //parse the data to json
+        .then(data => backgroundsArray = data.backgrounds)  //store the required data portion in an array
+        .then(() => backgroundsArray.forEach(function(background){createDropdown(background,'background_input')}))  //create a dropdown by iterating through the array, with each background name as a select option
 
 }
 
-//this creates the options for the input selection dropdown
+//Creates the options for the input selection dropdown
 function createDropdown(item, input){
     const option = document.createElement("option");
     option.textContent = item.name;
     document.getElementById(input).appendChild(option);
 }
 
+/*Prevents the form from default submitting on completion of a submit event
+  so that randomizer buttons can be clicked individually without submitting form */
+function stopSubmit(){
+    //get the form
+    const form = document.getElementById('form');
+
+    //prevent form from default submitting on completion
+    form.addEventListener("submit", function(event){
+        event.preventDefault();
+    })
+}
+
+/*Chooses a random option from the dropdown as the selected option,
+ then outputs the resulting selected option to the corresponding result box*/
 function randomSelector(input, output){
     //stop page from autosubmitting
     stopSubmit();
-
     //get the dropdown
     var dropDown = document.getElementById(input);
 
     //get possible races from dropdown
     var choices = dropDown.options;
 
-    //remove the Choose option from the choices
+    //remove the Choose option from the choices so that it's not included in the available choices
     if (choices[0].text.startsWith("Choose")) {
         choices.remove(0);
     }
-
-    //randomly get number for determining the index from the dropdown array
+    //randomly get number for determining the chosen index from the dropdown array
     let choiceSelector = Math.floor(Math.random() * choices.length);
 
-    //randomly selecting a race from the race array
+    //randomly selecting a race from the race array using the random index
     let randomSelection = choices[choiceSelector].text;
 
-    //set the randomized option as the option selected in the dropdown and output result
-    //dropDown.selectedIndex = choiceSelector;
+    //set the randomized option as the option selected in the dropdown then output result
     dropDown[choiceSelector].selected=true;
     document.getElementById(output).value=randomSelection;
 }
 
-//this function outputs the result to the result box
+//Rolls the dice to create a random result
+function roll(diceType, output){
+    //stop form from submitting
+    stopSubmit();
+
+    //get random number from the possible numbers on the dice, dependant on dice type (eg. D8 dice will have diceType=8)
+    let diceRoll = Math.floor(Math.random() * diceType);
+
+    //output the random number rolled to the corresponding result box
+    document.getElementById(output).value = diceRoll.toString();
+}
+
+//Outputs the selected dropdown value to the corresponding result box
 function fillResult(input, output){
     var dropDown = document.getElementById(input);
     var result = dropDown.options[dropDown.selectedIndex].text;
     document.getElementById(output).value=result;
 }
 
-//generates a random name, depending on the preselected race and gender of character
-function generateName(){
+/*******************************************************************************************/
+/*                              CHARACTER DETAILS                                          */
+/*******************************************************************************************/
 
+function getDetails(){
+
+    //get the values of the feature, specialty, trait, ideals, bond, flaw rolls
+    let background = document.getElementById('background_result').value;
+    let specialty = document.getElementById('specialty').value;
+    let trait = document.getElementById('trait').value;
+    let ideal = document.getElementById('ideal').value;
+    let bond = document.getElementById('bond').value;
+    let flaw = document.getElementById('flaw').value;
+
+    //declare variables for the storing the results of each character detail
+    let backgroundsArray, index, feature_result, specialty_result, trait_result, ideal_result, bond_result, flaw_result;
+
+    /*use fetch to get from the json file, then get data on the feature, specialty, trait, ideal, bond, and flaw details
+    according to the character background name inputted by user, or randomly rolled and selected*/
+    fetch("../json/backgrounds.json")
+        .then(res => res.json())
+        .then(data => backgroundsArray = data.backgrounds)
+        .then(()=> index = backgroundsArray.findIndex(obj => obj.name==background))
+        .then(()=> feature_result = backgroundsArray[index].feature)
+        .then(()=> document.getElementById('feature_result').innerHTML="Feature = "+feature_result)
+        .then(()=> specialty_result = backgroundsArray[index].specialty[specialty])
+        .then(()=> document.getElementById('specialty_result').innerHTML="Specialty = "+specialty_result)
+        .then(()=> trait_result = backgroundsArray[index].characteristics.trait[trait])
+        .then(()=> document.getElementById('trait_result').innerHTML="Trait = "+trait_result)
+        .then(()=> ideal_result = backgroundsArray[index].characteristics.ideal[ideal])
+        .then(()=> document.getElementById('ideal_result').innerHTML="Ideal = "+ideal_result)
+        .then(()=> bond_result = backgroundsArray[index].characteristics.bond[bond])
+        .then(()=> document.getElementById('bond_result').innerHTML="Bond = "+bond_result)
+        .then(()=> flaw_result = backgroundsArray[index].characteristics.flaw[flaw])
+        .then(()=> document.getElementById('flaw_result').innerHTML="Flaw = "+flaw_result)
+}
+
+/*******************************************************************************************/
+/*                              RANDOM NAME GENERATOR                                      */
+/*******************************************************************************************/
+
+//Generates a random name, depending on the preselected race and gender of character
+function generateName(){
     //stop form from submitting
     stopSubmit();
 
@@ -224,125 +292,8 @@ function generateName(){
     document.getElementById("input_name").value = randomName;
 }
 
-//this function prevents the form from default submitting on completion of a submit event
-function stopSubmit(){
-    //get the form
-    const form = document.getElementById('form');
-
-    //prevent form from default submitting on completion
-    form.addEventListener("submit", function(event){
-        event.preventDefault();
-    })
-}
-
-//this function rolls the dice to create a random result
-function roll(diceType, output){
-
-    //stop form from submitting
-    stopSubmit();
-
-    //get random number from the possible numbers on the dice, dependant on dice type
-    let diceRoll = Math.floor(Math.random() * diceType);
-
-    //output the result to the corresponding result box
-    document.getElementById(output).value = diceRoll.toString();
-}
-
-function getDetails(){
-
-    //get the values of the feature, specialty, trait, ideals, bond, flaw rolls
-    let background = document.getElementById('background_result').value;
-    let specialty = document.getElementById('specialty').value;
-    let trait = document.getElementById('trait').value;
-    let ideal = document.getElementById('ideal').value;
-    let bond = document.getElementById('bond').value;
-    let flaw = document.getElementById('flaw').value;
-
-
-    let backgroundsArray;
-    let index;
-    let feature_result;
-    let specialty_result;
-    let trait_result;
-    let ideal_result;
-    let bond_result;
-    let flaw_result;
-
-    /*use fetch to get from the json file to get the feature, specialty, trait, ideal, bond, and flaw details according to the values
-    inputted by user or randomly rolled and selected*/
-    fetch("../json/backgrounds.json")
-        .then(res => res.json())
-        .then(data => backgroundsArray = data.backgrounds)
-        .then(()=> index = backgroundsArray.findIndex(obj => obj.name==background))
-        .then(()=> feature_result = backgroundsArray[index].feature)
-        .then(()=> document.getElementById('feature_result').innerHTML="Feature = "+feature_result)
-        .then(()=> specialty_result = backgroundsArray[index].specialty[specialty])
-        .then(()=> document.getElementById('specialty_result').innerHTML="Specialty = "+specialty_result)
-        .then(()=> trait_result = backgroundsArray[index].characteristics.trait[trait])
-        .then(()=> document.getElementById('trait_result').innerHTML="Trait = "+trait_result)
-        .then(()=> ideal_result = backgroundsArray[index].characteristics.ideal[ideal])
-        .then(()=> document.getElementById('ideal_result').innerHTML="Ideal = "+ideal_result)
-        .then(()=> bond_result = backgroundsArray[index].characteristics.bond[bond])
-        .then(()=> document.getElementById('bond_result').innerHTML="Bond = "+bond_result)
-        .then(()=> flaw_result = backgroundsArray[index].characteristics.flaw[flaw])
-        .then(()=> document.getElementById('flaw_result').innerHTML="Flaw = "+flaw_result)
 
 
 
 
 
-
-/*
-    //get details, dependant on background type
-    if (background.toLowerCase() == "acolyte"){
-
-        if (trait == "1") document.getElementById('details').textContent += "Personality Trait: I idolize a particular hero of my faith, and constantly refer to that person’s deeds and example.";
-        else if (trait == "2") document.getElementById('details').textContent += "Personality Trait: I can find common ground between the fiercest enemies, empathizing with them and always working toward peace.";
-        else if (trait == "3") document.getElementById('details').textContent += "Personality Trait: I see omens in every event and action. The gods try to speak to us, we just need to listen.";
-        else if (trait == "4") document.getElementById('details').textContent += "Personality Trait: Nothing can shake my optimistic attitude.";
-        else if (trait == "5") document.getElementById('details').textContent += "Personality Trait: I quote (or misquote) sacred texts and proverbs in almost every situation.";
-        else if (trait == "6") document.getElementById('details').textContent += "Personality Trait: I am tolerant (or intolerant) of other faiths and respect (or condemn) the worship of other gods.";
-        else if (trait == "7") document.getElementById('details').textContent += "Personality Trait: I’ve enjoyed fine food, drink, and high society among my temple’s elite. Rough living grates on me.";
-        else if (trait == "8") document.getElementById('details').textContent += "Personality Trait: I’ve spent so long in the temple that I have little practical experience dealing with people in the outside world.";
-
-        if (ideal == "1") document.getElementById('details').textContent += "\nIdeal: I idolize a particular hero of my faith, and constantly refer to that person’s deeds and example.";
-        else if (ideal == "2") document.getElementById('details').textContent += "\nIdeal: Charity. I always try to help those in need, no matter what the personal cost. (Good)";
-        else if (ideal == "3") document.getElementById('details').textContent += "\nIdeal: Change. We must help bring about the changes the gods are constantly working in the world. (Chaotic)";
-        else if (ideal == "4") document.getElementById('details').textContent += "\nIdeal: Power. I hope to one day rise to the top of my faith’s religious hierarchy. (Lawful)";
-        else if (ideal == "5") document.getElementById('details').textContent += "\nIdeal: Faith. I trust that my deity will guide my actions. I have faith that if I work hard, things will go well. (Lawful)";
-        else if (ideal == "6") document.getElementById('details').textContent += "\nIdeal: Aspiration. I seek to prove myself worthy of my god’s favor by matching my actions against his or her teachings. (Any)";
-
-        if (bond == "1") document.getElementById('details').textContent += "\nBond: I would die to recover an ancient relic of my faith that was lost long ago.";
-        else if (bond == "2") document.getElementById('details').textContent += "\nBond: I will someday get revenge on the corrupt temple hierarchy who branded me a heretic.";
-        else if (bond == "3") document.getElementById('details').textContent += "\nBond: I owe my life to the priest who took me in when my parents died.";
-        else if (bond == "4") document.getElementById('details').textContent += "\nBond: Everything I do is for the common people.";
-        else if (bond == "5") document.getElementById('details').textContent += "\nBond: I will do anything to protect the temple where I served.";
-        else if (bond == "6") document.getElementById('details').textContent += "\nBond: I seek to preserve a sacred text that my enemies consider heretical and seek to destroy.";
-
-        if (flaw == "1") document.getElementById('details').textContent += "\nFlaw: I judge others harshly, and myself even more severely.";
-        else if (flaw == "2") document.getElementById('details').textContent += "\nFlaw: I put too much trust in those who wield power within my temple’s hierarchy.";
-        else if (flaw == "3") document.getElementById('details').textContent += "\nFlaw: My piety sometimes leads me to blindly trust those that profess faith in my god.";
-        else if (flaw == "4") document.getElementById('details').textContent += "\nFlaw: I am inflexible in my thinking.";
-        else if (flaw == "5") document.getElementById('details').textContent += "\nFlaw: I am suspicious of strangers and expect the worst of them.";
-        else if (flaw == "6") document.getElementById('details').textContent += "\nFlaw: Once I pick a goal, I become obsessed with it to the detriment of everything else in my life.";
-
-    }
-
-    else if (background.toLowerCase() == "criminal/spy"){
-
-    }
-    else if (background.toLowerCase() == "folk hero"){
-
-    }
-    else if (background.toLowerCase() == "noble"){
-
-    }
-    else if (background.toLowerCase() == "sage"){
-
-    }
-    else if (background.toLowerCase() == "soldier"){
-
-    }*/
-
-
-}
